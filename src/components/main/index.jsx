87b010bcd68ddf8565b00a6d4dc67d10704870ad
Eugenia Smith <ev-genius4@mail.ru>
2024-08-14 14:02:58 +0200

    sponsors component is created, installed and styled

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.css";
 import Banner from "./banner";
+import Sponsors from "./sponsors";
 
 function Main() {
   return (
     <div className={styles.main_wrapper}>
       <Banner />
+      <Sponsors />
     </div>
   );
 }
---
+++ src/components/main/sponsors/index.jsx
@@ -0,0 +1,33 @@
+import styles from "./styles.module.css";
+import cola from "../../../assets/icons/coca_cola_logo_item.svg";
+import disney from "../../../assets/icons/disney_logo_icon.svg";
+import microsoft from "../../../assets/icons/microsoft_logo_icon.svg";
+import facebook from "../../../assets/icons/facebook_logo_icon.svg";
+import sony from "../../../assets/icons/sony_logo_icon.svg";
+
+function Sponsors() {
+  return (
+    <section className={styles.sponsors_container}>
+      <h2 className={styles.section_heading}>Our clients are:</h2>
+      <ul className={styles.logo_container}>
+        <li className={styles.logo}>
+          <img src={disney} alt="Disney logo" />
+        </li>
+        <li className={styles.logo}>
+          <img src={facebook} alt="Facebook logo" />
+        </li>
+        <li className={styles.logo}>
+          <img src={microsoft} alt="Microsoft logo" />
+        </li>
+        <li className={styles.logo}>
+          <img src={sony} alt="Sony logo" />
+        </li>
+        <li className={styles.logo}>
+          <img src={cola} alt="Coca-Cola logo" />
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Sponsors;
